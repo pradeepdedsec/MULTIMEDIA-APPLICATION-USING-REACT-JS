162a5eb63ac4d6e49c68b2d36608048ac942a762
pradeepdedsec <pradeepdedsec@gmail.com>
2024-09-22 23:51:45 +0530

    english  movie section

--- src/Components/DynamicImageBox.js
+++ src/Components/DynamicImageBox.js
@@ -1,14 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../Styles/dynamicImageBox.css";
 import DynamicImageClick from './DynamicImageClick'
 
 const DynamicImageBox = ({list,goToPage}) => {
     console.log("dynamic"+list);
+
+    const[dynamicList,setDynamicList]=useState([
+
+        {
+            Genre:"New Movies",
+            photos:[
+                {
+                    name:"AR_Rahman",
+                    path:"Loading/Loading.gif"
+                },
+                {
+                    name:"G.V.Prakash",
+                    path:"Loading/Loading.gif"
+                },
+                {
+                    name:"Aniruth",
+                    path:"Loading/Loading.gif"
+                },
+                {
+                    name:"VijayAntony",
+                    path:"Loading/Loading.gif"
+                },
+                {
+                    name:"YuvanShankarraja",
+                    path:"Loading/Loading.gif"
+                },
+        
+            ]
+        },
+        {
+          Genre:"Upcoming Movies",
+          photos:[
+            {
+                name:"AR_Rahman",
+                path:"Loading/Loading.gif"
+            },
+            {
+                name:"G.V.Prakash",
+                path:"Loading/Loading.gif"
+            },
+            {
+                name:"Aniruth",
+                path:"Loading/Loading.gif"
+            },
+            {
+                name:"VijayAntony",
+                path:"Loading/Loading.gif"
+            },
+            {
+                name:"YuvanShankarraja",
+                path:"Loading/Loading.gif"
+            },
+      
+          ]
+      }
+      ]);
+
+      useEffect(()=>{
+        setDynamicList(list)
+      },[list])
+
+
+
+
+
+
+
   return (
     <>
         <div className='whole-music-box'>
             {
-                list.map((e,i)=>(
+                dynamicList.map((e,i)=>(
                     <>
                         <div className='total-scroll-box'>
                             <h1 className='scroll-box-head'>{e.Genre}</h1>
